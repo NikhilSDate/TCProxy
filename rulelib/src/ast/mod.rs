b7e92c13e7e172c0b3a9aac410f6d2a6104475ec
Ethan Zhang <ethanz4@illinois.edu>
2024-10-05 16:05:27 -0500

    implement parsing for ProxyMode type

--- rulelib/src/ast/mod.rs
+++ rulelib/src/ast/mod.rs
@@ -1,11 +1,28 @@
+use pest::iterators::Pair;
+use crate::Rule;
+
+#[derive(Debug)]
 pub enum ProxyMode {
     OPAQUE,
     TRANSPARENT,
 }
 
+impl TryFrom<&str> for ProxyMode {
+    type Error = AstParseError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "OPAQUE" => Ok(ProxyMode::OPAQUE),
+            "TRANSPARENT" => Ok(ProxyMode::TRANSPARENT),
+            _ => Err(AstParseError::ParseError(format!("Unknown proxy mode: {}", value)))
+        }
+    }
+}
+
 /// These are all meant to be "special forms," which have a different order of evaluation from typical terms;
 /// for example, `(if a b c)` should only execute *either* the consequent or the alternative, depending on the truth value of `a`
 /// See: https://www.cs.cmu.edu/Groups/AI/html/cltl/clm/node59.html
+#[derive(Debug)]
 pub enum SpecialForms {
     /// (if <predicate> <consequent> <alternative>)
     If {
@@ -30,6 +47,8 @@ pub enum SpecialForms {
     },
 }
 
+
+#[derive(Debug)]
 pub enum RuleOutcome {
     /// Silently drop the inbound packet
     DROP,
@@ -50,6 +69,7 @@ pub enum RuleOutcome {
     CONTINUE,
 }
 
+#[derive(Debug)]
 pub enum BuiltinOp {
     Func {
         name: String,
@@ -59,11 +79,50 @@ pub enum BuiltinOp {
     Outcome(RuleOutcome),
 }
 
+#[derive(Debug)]
+pub enum List<T> {
+    Nil,
+    NonEmpty(Vec<T>),
+}
+
+#[derive(Debug)]
 pub enum AstNode {
     // TODO: find a better name for this
     Keyword(BuiltinOp),
     Num(i64),
     Ident(String),
     String(String),
-    Sexp(Vec<Box<AstNode>>)
+    Sexp(List<Box<AstNode>>),
+}
+
+#[derive(Debug)]
+pub enum AstParseError {
+    ParseError(String),
+}
+
+#[cfg(test)]
+mod tests {
+    mod proxy_mode {
+        use crate::ast::{AstParseError, ProxyMode};
+
+        #[test]
+        fn try_from_works_on_valid_strings() {
+            let proxy_mode = ProxyMode::try_from("OPAQUE");
+            assert!(matches!(proxy_mode, Ok(ProxyMode::OPAQUE)));
+
+            let proxy_mode = ProxyMode::try_from("TRANSPARENT");
+            assert!(matches!(proxy_mode, Ok(ProxyMode::TRANSPARENT)));
+        }
+
+        #[test]
+        fn try_from_fails_on_invalid_strings() {
+            // bad capitalization?
+            let proxy_mode = ProxyMode::try_from("OPaQUE");
+            assert!(matches!(proxy_mode, Err(AstParseError::ParseError(_))));
+
+            // random string?
+            let proxy_mode = ProxyMode::try_from("oeau");
+            assert!(matches!(proxy_mode, Err(AstParseError::ParseError(_))));
+        }
+    }
 }
