f25a2298924c9896a69c87a5ec3d5b571ba43229
Ethan Zhang <ethanz4@illinois.edu>
2024-10-05 16:47:10 -0500

    WIP parsing for special forms

--- rulelib/src/ast/mod.rs
+++ rulelib/src/ast/mod.rs
@@ -1,7 +1,7 @@
 use pest::iterators::Pair;
 use crate::Rule;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum ProxyMode {
     OPAQUE,
     TRANSPARENT,
@@ -22,7 +22,7 @@ impl TryFrom<&str> for ProxyMode {
 /// These are all meant to be "special forms," which have a different order of evaluation from typical terms;
 /// for example, `(if a b c)` should only execute *either* the consequent or the alternative, depending on the truth value of `a`
 /// See: https://www.cs.cmu.edu/Groups/AI/html/cltl/clm/node59.html
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum SpecialForms {
     /// (if <predicate> <consequent> <alternative>)
     If {
@@ -47,8 +47,76 @@ pub enum SpecialForms {
     },
 }
 
+// TODO: refactor
+impl TryFrom<Pair<'_, Rule>> for SpecialForms {
+    type Error = AstParseError;
 
-#[derive(Debug)]
+    /// Tries to convert a parse tree node to a SpecialForm.
+    /// Expects an `s_expr` as input
+    fn try_from(value: Pair<Rule>) -> Result<Self, Self::Error> {
+        // TODO: convert ParseError messages to enums
+        match value.as_rule() {
+            Rule::s_exp => Self::try_from(value.into_inner().next().expect("an `s_exp` is always either `list` or `ident`")),
+            Rule::list => {
+                let inner = value.into_inner().next();
+                match inner {
+                    // `nil`
+                    None => Err(AstParseError::ParseError("expected `list_part`, found `nil`".to_string())),
+                    // `list_part`
+                    Some(inner) => Self::try_from(inner)
+                }
+            }
+            Rule::list_part => {
+                let inner: Vec<_> = value.into_inner().collect();
+                match inner.first() {
+                    None => unreachable!("`list_part` always contains at least one child"),
+                    Some(expr) => {
+                        match expr.as_str() {
+                            "if" => {
+                                // "if" + predicate + consequent + alternative
+                                if inner.len() != 4 {
+                                    Err(AstParseError::ParseError(format!("wrong arity for if; expected 3, received {}", inner.len() - 1)))
+                                } else {
+                                    // I think the clone here is necessary
+                                    let predicate = Box::new(AstNode::try_from(inner[1].clone())?);
+                                    let consequent = Box::new(AstNode::try_from(inner[2].clone())?);
+                                    let alternative = Box::new(AstNode::try_from(inner[3].clone())?);
+
+                                    Ok(Self::If { predicate, consequent, alternative })
+                                }
+                            }
+                            "def-var" => {
+                                // "def-var" + name + value
+                                if inner.len() != 3 {
+                                    Err(AstParseError::ParseError(format!("wrong arity for def-var; expected 2, received {}", inner.len() - 1)))
+                                } else {
+                                    // if (inner[1].as_rule != )
+
+                                    // encore un fois (see comment in "if")
+                                    todo!()
+                                }
+                            }
+                            "def-rule" => { todo!() }
+                            "set-mode" => {
+                                // set-mode + OPAQUE/TRANSPARENT
+                                if inner.len() != 2 {
+                                    Err(AstParseError::ParseError(format!("wrong arity for set-mode; expected 1, received {}", inner.len() - 1)))
+                                } else {
+                                    let mode = ProxyMode::try_from(inner[1].as_str())?;
+                                    Ok(Self::SetMode { mode })
+                                }
+                            }
+                            _ => Err(AstParseError::ParseError(format!("expected a special form, received {}", expr.as_str())))
+                        }
+                    }
+                }
+            }
+            rule => Err(AstParseError::ParseError(format!("expected `s_expr`, received {:?}", rule))),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum RuleOutcome {
     /// Silently drop the inbound packet
     DROP,
@@ -69,7 +137,7 @@ pub enum RuleOutcome {
     CONTINUE,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum BuiltinOp {
     Func {
         name: String,
@@ -79,13 +147,13 @@ pub enum BuiltinOp {
     Outcome(RuleOutcome),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum List<T> {
     Nil,
     NonEmpty(Vec<T>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum AstNode {
     // TODO: find a better name for this
     Keyword(BuiltinOp),
@@ -95,18 +163,34 @@ pub enum AstNode {
     Sexp(List<Box<AstNode>>),
 }
 
-#[derive(Debug)]
+impl TryFrom<Pair<'_, Rule>> for AstNode {
+    type Error = AstParseError;
+
+    fn try_from(value: Pair<'_, Rule>) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum AstParseError {
     ParseError(String),
 }
 
 #[cfg(test)]
 mod tests {
+    // FIXME: leaky unit tests, but I don't want to manually write out parse trees...
+    use pest::Parser;
+    use pest_derive::Parser;
+    use crate::{Rule, RuleParser};
+
+    use crate::ast::AstParseError;
+
     mod proxy_mode {
-        use crate::ast::{AstParseError, ProxyMode};
+        use super::*;
+        use crate::ast::ProxyMode;
 
         #[test]
-        fn try_from_works_on_valid_strings() {
+        fn try_from__works_on_valid_strings() {
             let proxy_mode = ProxyMode::try_from("OPAQUE");
             assert!(matches!(proxy_mode, Ok(ProxyMode::OPAQUE)));
 
@@ -115,7 +199,7 @@ mod tests {
         }
 
         #[test]
-        fn try_from_fails_on_invalid_strings() {
+        fn try_from__fails_on_invalid_strings() {
             // bad capitalization?
             let proxy_mode = ProxyMode::try_from("OPaQUE");
             assert!(matches!(proxy_mode, Err(AstParseError::ParseError(_))));
@@ -123,6 +207,124 @@ mod tests {
             // random string?
             let proxy_mode = ProxyMode::try_from("oeau");
             assert!(matches!(proxy_mode, Err(AstParseError::ParseError(_))));
+        }
+    }
+
+    mod special_forms {
+        use super::*;
+        use crate::ast::{ProxyMode, SpecialForms};
+
+        #[test]
+        fn try_from__fails_on_unexpected_parse_trees() {
+            let parse_tree = RuleParser::parse(Rule::s_exp, "100").unwrap().next().unwrap();
+            let ast = SpecialForms::try_from(parse_tree);
+            assert!(ast.is_err());
+
+            let parse_tree = RuleParser::parse(Rule::s_exp, "hi").unwrap().next().unwrap();
+            let ast = SpecialForms::try_from(parse_tree);
+            assert!(ast.is_err());
+
+            let parse_tree = RuleParser::parse(Rule::s_exp, "(bob was here)").unwrap().next().unwrap();
+            let ast = SpecialForms::try_from(parse_tree);
+            assert!(ast.is_err());
+
+            let parse_tree = RuleParser::parse(Rule::s_exp, "(cow 100)").unwrap().next().unwrap();
+            let ast = SpecialForms::try_from(parse_tree);
+            assert!(ast.is_err());
+        }
+
+
+        mod set_mode {
+            use super::*;
+
+            #[test]
+            fn try_from__works_with_expected_parse_trees() {
+                let parse_tree = RuleParser::parse(Rule::s_exp, "(set-mode TRANSPARENT)").unwrap().next().unwrap();
+
+                let ast = SpecialForms::try_from(parse_tree).unwrap();
+                assert!(matches!(ast, SpecialForms::SetMode { mode: ProxyMode::TRANSPARENT }));
+            }
+
+            #[test]
+            fn try_from__fails_on_parse_tree_with_wrong_arity() {
+                let parse_tree = RuleParser::parse(Rule::s_exp, "(set-mode OPAQUE hi)").unwrap().next().unwrap();
+
+                let ast = SpecialForms::try_from(parse_tree);
+                assert!(ast.is_err());
+            }
+
+            #[test]
+            fn try_from__fails_on_well_formed_parse_tree_with_unexpected_argument() {
+                let parse_tree = RuleParser::parse(Rule::s_exp, "(set-mode CANDY)").unwrap().next().unwrap();
+
+                let ast = SpecialForms::try_from(parse_tree);
+                assert!(ast.is_err());
+            }
+        }
+
+        mod r#if {
+            use super::*;
+
+            #[test]
+            #[ignore]
+            fn try_from__works_with_expected_parse_trees() {
+                todo!()
+            }
+
+            #[test]
+            #[ignore]
+            fn try_from__fails_on_parse_tree_with_wrong_arity() {
+                todo!()
+            }
+
+            #[test]
+            #[ignore]
+            fn try_from__fails_on_well_formed_parse_tree_with_unexpected_argument() {
+                todo!()
+            }
+        }
+        mod def_var {
+            use super::*;
+
+            #[test]
+            #[ignore]
+            fn try_from__works_with_expected_parse_trees() {
+                todo!()
+            }
+
+            #[test]
+            #[ignore]
+            fn try_from__fails_on_parse_tree_with_wrong_arity() {
+                todo!()
+            }
+
+            #[test]
+            #[ignore]
+            fn try_from__fails_on_well_formed_parse_tree_with_unexpected_argument() {
+                todo!()
+            }
+        }
+
+        mod def_rule {
+            use super::*;
+
+            #[test]
+            #[ignore]
+            fn try_from__works_with_expected_parse_trees() {
+                todo!()
+            }
+
+            #[test]
+            #[ignore]
+            fn try_from__fails_on_parse_tree_with_wrong_arity() {
+                todo!()
+            }
+
+            #[test]
+            #[ignore]
+            fn try_from__fails_on_well_formed_parse_tree_with_unexpected_argument() {
+                todo!()
+            }
         }
     }
 }
