957c709bdd49da3a527f63d8b2ba086b57e40ce3
Nikhil Date <nikhil.s.date@gmail.com>
2024-10-05 17:47:25 -0500

    make atom non-atomic

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,15 +1,22 @@
 // playground for informal testing
-use rulelib::RuleParser;
-use rulelib::Rule;
 use pest::Parser;
+use rulelib::Rule;
+use rulelib::RuleParser;
 
 fn main() {
-    let text = "(def-var bad-ip \"192.0.0.1\")";
-    let string = "\"192.0.0.1\"";
-    let atom = "def-var";
-    let s_expr = "(def-var)";
-    let cons = "(def-var bad-ip (1 2))";
-    let complex_rule = "(def-rule simple-rule (:target \"127.0.0.1\" :port \"80\") (if (and (exact? metadata-source bad-ip) (exact? metedata-dest   :target) DROP REDIRECT)))";
-    let parse_result = RuleParser::parse(Rule::program, complex_rule);
+    let program = r#"
+        (def-var bad-ip "192.0.1.2")
+
+        (def-rule simple-rule (:target "127.0.0.1" :port   "80")
+            (if (and (exact? metadata-source bad-ip) (exact? metedata-dest   :target)
+                DROP
+                REDIRECT)))
+
+        (def-rule simple-rewrite
+            (if (and (exact? metadata-source bad-ip) (matches? content "foo"))
+                (REWRITE "^bar$" "baz")
+                (simple-rule)))
+    "#;
+    let parse_result = RuleParser::parse(Rule::program, program);
     println!("{}", parse_result.unwrap());
 }
